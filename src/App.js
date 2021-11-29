130dfe461274ef81585b397672aa245cc558b413
Tatiana <hello@tatianacodes.com>
2021-11-28 19:44:43 -0800

    Update button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import * as React from "react";
+import {useEffect, useState} from 'react';
 import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+ const [currentAccount, setCurrentAccount] = useState("");
 
- const checkIfWalletIsConnect = () => {
-   const {ethereum} = window;
+ const checkIfWalletIsConnected = async () => {
+  try {
+ const {ethereum} = window;
    if(!ethereum){
      console.log("Make sure you have metamask!")
      return;
    } else {
      console.log("We have the ethereum object")
-   }
+    }
+    const accounts = await ethereum.request({
+      method: 'eth_accounts'
+    })
+    if(accounts.length !== 0){
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+    } else {
+      console.log("No authorized account found")
+    }
+  } catch(error) {
+    console.log(error)
+  }
  }
-    
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    } catch (error) {
+      console.log(error)
+    }
+  } 
   useEffect(() => {
-    checkIfWalletIsConnect()
+    checkIfWalletIsConnected()
   }, [])
   
   return (
@@ -30,9 +62,14 @@ export default function App() {
         Hi I'm Tatiana and I'm looking to get going with understanding Web3!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+     {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
